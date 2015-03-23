69b00b8dd90832de1ab3978b2dfb4e7c85c299bb
Sergio Tapia <sergiotapia@outlook.com>
2015-03-22 21:17:33 -0400

    Basic schema for database.

---
+++ db/migrate/20150323004827_create_initial_models.rb
@@ -0,0 +1,43 @@
+class CreateInitialModels < ActiveRecord::Migration
+  def change
+    create_table :categories do |t|
+      t.string :name
+      t.timestamps
+    end
+
+    create_table :subcategories do |t|
+      t.string :name
+      t.integer :category_id, index: true
+      t.timestamps
+    end
+
+    create_table :listing do |t|
+      t.integer :subcategory_id, index: true
+      t.string :name
+      t.text :description
+      t.decimal :price, precision: 8, scale: 2
+      t.string :contact_name
+      t.string :email
+      t.string :telephone
+      t.integer :location_id, index: true
+      t.timestamps
+    end
+
+    create_table :locations do |t|
+      t.string :name
+      t.timestamps
+    end
+
+    create_table :favorited_listings do |t|
+      t.integer :listing_id, index: true
+      t.integer :user_id, index: true
+      t.timestamps
+    end
+
+    create_table :reported_listings do |t|
+      t.integer :listing_id, index: true
+      t.integer :user_id, index: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,66 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150323004827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "favorited_listings", force: :cascade do |t|
+    t.integer  "listing_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "favorited_listings", ["listing_id"], name: "index_favorited_listings_on_listing_id", using: :btree
+  add_index "favorited_listings", ["user_id"], name: "index_favorited_listings_on_user_id", using: :btree
+
+  create_table "listing", force: :cascade do |t|
+    t.integer  "subcategory_id"
+    t.string   "name"
+    t.text     "description"
+    t.decimal  "price",          precision: 8, scale: 2
+    t.string   "contact_name"
+    t.string   "email"
+    t.string   "telephone"
+    t.integer  "location_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "listing", ["location_id"], name: "index_listing_on_location_id", using: :btree
+  add_index "listing", ["subcategory_id"], name: "index_listing_on_subcategory_id", using: :btree
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reported_listings", force: :cascade do |t|
+    t.integer  "listing_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "reported_listings", ["listing_id"], name: "index_reported_listings_on_listing_id", using: :btree
+  add_index "reported_listings", ["user_id"], name: "index_reported_listings_on_user_id", using: :btree
+
+  create_table "subcategories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "subcategories", ["category_id"], name: "index_subcategories_on_category_id", using: :btree
 
 end
