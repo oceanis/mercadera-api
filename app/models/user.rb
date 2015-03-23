98811cd78c2c1b3b4a55093f4c4555a2336292e5
Sergio Tapia <sergiotapia@outlook.com>
2015-03-22 21:52:04 -0400

    On user create, make sure we generate an API key.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   has_many :listings
+
+  before_create :generate_api_token
+
+  private
+
+  def generate_api_token
+    begin
+      self.api_token = SecureRandom.hex
+    end while self.class.exists?(api_token: api_token)
+  end
 end
