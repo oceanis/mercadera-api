903ae968908335aa9f0db55ab040249949a0f0d5
Sergio Tapia <sergiotapia@outlook.com>
2015-03-23 10:49:55 -0400

    Add Location to seeds files.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
 User.destroy_all
+Location.destroy_all
 
 User.create!(api_token: SecureRandom.hex, name: 'Sergio Tapia', role: 'Admin')
+
+Location.create!(name: 'Santa Cruz')
+Location.create!(name: 'Cochabamba')
+Location.create!(name: 'La Paz')
+Location.create!(name: 'Tarija')
+Location.create!(name: 'Beni')
+Location.create!(name: 'Pando')
+Location.create!(name: 'Oruro')
+Location.create!(name: 'Chuquisaca')
+Location.create!(name: 'Potosi')
