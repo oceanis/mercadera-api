e67986024d1bd893e5132ab8e41cff2170b0f02c
Sergio Tapia <sergiotapia@outlook.com>
2015-03-26 11:24:19 -0400

    Remove API authorization temporarily from the app for development.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
 
   def authorize_api_key
-    if Rails.env == 'production'
-      user = User.find_by(api_token: params[:token])
-      head :unauthorized unless user
-    end
+    # if Rails.env == 'production'
+    #   user = User.find_by(api_token: params[:token])
+    #   head :unauthorized unless user
+    # end
   end
 end
